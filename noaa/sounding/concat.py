1a66223c3ce2acbfd6a9de7e925e9e9e7e18c93c
robertorivera36 <31109974+robertorivera36@users.noreply.github.com>
2020-06-12 15:26:34 -0400

    Update concat.py

--- noaa/sounding/concat.py
+++ noaa/sounding/concat.py
@@ -1,35 +1,100 @@
 import os
+import numpy as np
 
-inPath = 'C:/Users/Roberto/Documents/Climatología/Sounding/processed_soundings_1982-2018/tjsj_soundings_1982-2018/dailyData/'
-outPath = 'C:/Users/Roberto/Documents/Climatología/Sounding/processed_soundings_1982-2018/tjsj_soundings_1982-2018/dailyData/concat/'
+def cleanFile():
+	#--- Opens file as readable then copies data to y, then closes file
+	data = open(wpath+filename, 'r')
+	y = data.read()
+	data.close()
 
-outputFileName = 'concatSoundings_obs.csv'
+	#--- Reopens file as writable
+	data = open(wpath+filename, 'w')
 
-unwantedLine = "STATIONID,DATE,TIME,PRES,HGHT,TEMP,DWPT,RELH,MIXR,DRCT,SKNT,THTA,THTE,THTV\n"
-unwantedLine1 = "VARCHAR(4),YYYY-MM-DD,HHMMSS,hPa,m,C,C,%,g/kg,deg,knot,K,K,K\n"
+	#--- test
+	#print(y)
+
+	y = y.replace(', ', ',').replace(' ,',',')
+
+	#--- test
+	#print(y)
+
+	#--- Write data from y into file
+	data.write(y)
+	    
+	data.close()
+
+	print(filename+' cleaned')
+
+#--- Opens and transposes file
+def transpose():
+	x = np.loadtxt(fname = wpath+filename, dtype='str', delimiter = ',')
+	x = np.transpose(x)
+	np.savetxt(filename, x, fmt='%s', delimiter=",")
+
+	print(filename+' Transposed')
+
+#--- Working path
+wpath = 'C:/Users/Roberto/Desktop/dailyData/'
+outPath = 'C:/Users/Roberto/Desktop/'
+
+outputFileName = 'concatSoundings_obs_pre-1989.csv'
+outputFileName1 = 'concatSoundings_obs_post-1989.csv'
+outputFileNameInd = 'concatIndexes.csv'
+
+#--- Pre-1989 Soundings Variables
+header = "STATIONID,DATE,TIME,PRES,HGHT,TEMP,DWPT,RELH,MIXR,DRCT,SKNT,THTA,THTE,THTV\n"
+variableTypes = "VARCHAR(4),YYYY-MM-DD,HHMMSS,hPa,m,C,C,%,g/kg,deg,knot,K,K,K\n"
+
+#--- Post-1989 Sounding Variables
+header1 = "STATIONID,DATE,TIME,PRES,HGHT,TEMP,DWPT,FRPT,RELH,RELI,MIXR,DRCT,SKNT,THTA,THTE,THTV\n"
+variableTypes1 = "VARCHAR(4),YYYY-MM-DD,HHMMSS,hPa,m,C,C,C,%,%,g/kg,deg,knot,K,K,K\n"
+
+#--- Index variables
+indexHeader = "Station identifier,Station number,Observation time,Station latitude,Station longitude,Station elevation,Showalter index,Lifted index,LIFT computed using virtual temperature,SWEAT index,K index,Cross totals index,Vertical totals index,Totals totals index,Convective Available Potential Energy,CAPE using virtual temperature,Convective Inhibition,CINS using virtual temperature,Equilibrum Level,Equilibrum Level using virtual temperature,Level of Free Convection,LFCT using virtual temperature,Bulk Richardson Number,Bulk Richardson Number using CAPV,Temp [K] of the Lifted Condensation Level,Pres [hPa] of the Lifted Condensation Level,Mean mixed layer potential temperature,Mean mixed layer mixing ratio,1000 hPa to 500 hPa thickness,Precipitable water [mm] for entire sounding\n"
 
 count = 0
+count1 = 0
+indexCount = 0
 
-directory = os.fsencode(inPath)
+post89 = False
 
-with open(outPath+outputFileName, 'w') as outfile:
+directory = os.fsencode(wpath)
+
+#with open(outPath+outputFileName, 'w') as outfile:
+with open(outPath+outputFileName, 'w') as outfile, open(outPath+outputFileName1, 'w') as outfile1, open(outPath+outputFileNameInd, 'w') as indexOutfile:
 	for file in os.listdir(directory):
 		filename = os.fsdecode(file)
 		if filename.endswith("_obs.txt"):
 			print (filename)
-			print(count)
 			with open(filename) as infile:
 				for line in infile:
-					#if ('STATIONID' in line) and count == 0:
-					if (line == unwantedLine or line == unwantedLine1) and count == 0:
+					if (line == header) and count == 0:
 						#print(line)
 						outfile.write(line)
 						count = count + 1
-					#elif 'STATIONID' in line:
-					elif line == unwantedLine or line == unwantedLine1:
+					elif (line == header1) and count1 == 0:
+						outfile1.write(line)
+						count1 = count1 + 1
+						post89 = True
+					elif line == header or line == variableTypes or line == header1 or line == variableTypes1:
 						continue
 					else:
 						#print(line)
-						outfile.write(line)
-		else:
-			continue
+						if post89 == False:
+							outfile.write(line)
+						elif post89 == True:
+							outfile1.write(line)
+
+		elif filename.endswith("_indices.txt"):
+			print (filename)
+			cleanFile()
+			transpose()
+			with open(filename) as infile:
+				for line in infile:
+					if line == indexHeader and indexCount == 0:
+						indexOutfile.write(line)
+						indexCount = indexCount + 1
+					elif line == indexHeader:
+						continue
+					else:
+						indexOutfile.write(line)
